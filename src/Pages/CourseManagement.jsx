17c344686980bb4c33d27ce59a89744dedfb714e
IshuRanaweera <ieshanibu@gmail.com>
2026-01-12 12:09:51 +0530

    crate heder

--- src/Pages/CourseManagement.jsx
+++ src/Pages/CourseManagement.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function CourseManagement() {
   return (
-    <div>
-      <h1>CourseManagement</h1>
+    <div className="bg-[#D2D2D2] h-screen overflow-y-auto ">
+      {/* Student Registration Form */}
+      <h1 className="text-2xl font-bold text-[#05003C] bg-[#878788] h-15 text-center p-2">
+        Student Registration
+      </h1>
     </div>
   )
 }
