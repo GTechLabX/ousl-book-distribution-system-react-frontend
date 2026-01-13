fb23d18813635bf6c29876591115bbb782bf4981
IshuRanaweera <ieshanibu@gmail.com>
2026-01-13 14:28:09 +0530

    edit bg colour

--- src/Pages/CourseManagement.jsx
+++ src/Pages/CourseManagement.jsx
@@ -2,12 +2,23 @@ import React from 'react'
 
 function CourseManagement() {
   return (
-    <div className="bg-[#D2D2D2] overflow-y-auto ">
-      {/* Student Registration Form */}
+   
+    <div className="bg-[#D2D2D2]  h-screen overflow-y-auto">
+      
       <h1 className="text-2xl font-bold text-[#05003C] bg-[#878788] h-15 text-center p-2">
-        Student Registration
-      </h1>
+        Course management
+      </h1><br />
+
+      <div className='w-full  p-2 bg-[#BEC4C8] flex justify-end rounded-xl '>
+          <button
+              type="submit"
+              className="px-7 py-2 bg-[#070055] text-white font-medium rounded-lg  focus:outline-none focus:ring-2 focus:ring-blue-500"
+            >
+              Register
+            </button>
+        </div>
     </div>
+    
   )
 }
 
